e54994dd296270ec89d428130edd397ac17c2282
rooiss <louis.kim010@gmail.com>
2020-04-14 14:55:26 -0700

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './styles/App.css';
+import PokeList from './PokeList'
+import DetailView from './DetailView'
+import Pokemon from '../Pokemon'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      pokemon: {}
+    }
+    this.handleOnClick = this.handleOnClick.bind(this)
+  }
+
+  handleOnClick(id) {
+    fetch(`http://pokeapi.co/api/v2/pokemon/${id}`)
+      .then(res => res.json())
+      .then(data => {
+        const pokemon = new Pokemon(data)
+        this.setState({pokemon})
+        console.log(pokemon)
+      })
+      .catch(err => console.log(err))
+  }
+
+  render(){
+    return (
+      <div className='App'>
+        <PokeList handleOnClick={this.handleOnClick}/>
+        <DetailView pokemon={this.state.pokemon}/>
+      </div>
+    )
+  }
+}
+
+export default App;
